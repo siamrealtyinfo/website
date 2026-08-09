c12d4147acb3941e19661a34121422d22337ed71
siamrealtyinfo <siamrealtyinfo@gmail.com>
2026-08-09 17:27:30 +0900

    エリア固定条件追加

--- Back/search_before_JIMDO_embedding.js
+++ Back/search_before_JIMDO_embedding.js
@@ -11,11 +11,11 @@ fetch(API_URL)
     // 4．HTMLに表示設定
     .then(
         properties => {
-            const filteredProperties = properties.filter(property => {
-                return  property.area =="Rama IX";
-            });
-
-            for (const property of filteredProperties){
+        //    console.log(properties);
+        //    propertylist.innerHTML = properties[0].name;
+            for (const property of properties){
+                
+        //      propertylist.innerHTML += property.name + "<br>"; 
                 const cardElement = document.createElement("div");
                 cardElement.className = "property-card";
 
@@ -34,6 +34,12 @@ fetch(API_URL)
                 titleElement.textContent = property.name;
                 cardElement.appendChild(titleElement);
                 
+                /*
+                const rentElement = document.createElement("p");
+                rentElement.textContent = property.rent + " THB";
+                cardElement.appendChild(rentElement);
+                */
+
                 const descriptionElement = document.createElement("p");
                 descriptionElement.className = "property-description";
                 descriptionElement.textContent = property.description;
@@ -51,3 +57,44 @@ fetch(API_URL)
         }
     );
 
+
+
+/*
+const propertyList = document.getElementById("property-list");
+ propertylist.innerHTML="<h2>Hello Property!</h2>";
+
+const properties=[
+    {
+        name: "Belle Grand Rama 9",
+        rent: "25,000 THB",
+        image: "https://image.jimcdn.com/app/cms/image/transf/dimension=480x1024:format=jpg/path/s805fd7c389cf4759/image/ib88d90917d813487/version/1772163956/image.jpg"
+    },
+    {
+        name: "Life Asoke Rama 9",
+        rent: "22,000 THB",
+        image: "https://image.jimcdn.com/app/cms/image/transf/dimension=480x1024:format=jpg/path/s805fd7c389cf4759/image/i6a0d866da5369710/version/1772163929/image.jpg"
+    },
+    {
+        name: "One 9 Five",
+        rent: "30,000 THB",
+        image: "https://image.jimcdn.com/app/cms/image/transf/dimension=480x1024:format=jpg/path/s805fd7c389cf4759/image/ib1e057d78df915cb/version/1772167091/image.jpg"
+    }
+];
+
+for (const property of properties){
+    const card=document.createElement("div");
+    card.className="property-card";
+    const title=document.createElement("h2");
+    title.textContent=property.name;
+    const rent=document.createElement("p");
+    rent.textContent=property.rent; 
+    const image=document.createElement("img");
+    image.src=property.image;
+
+    card.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(rent);
+
+    propertylist.appendChild(card);
+};
+*/
