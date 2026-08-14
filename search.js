3403abb6bb6a28dea8e9c4132d67eaeb62db2989
siamrealtyinfo <siamrealtyinfo@gmail.com>
2026-08-14 13:58:44 +0900

    家賃非表示に設定

--- search.js
+++ search.js
@@ -43,11 +43,12 @@ function renderProperties(properties){
         titleElement.textContent = property.name;
         cardElement.appendChild(titleElement);
         
-        // 家賃
+/*        // 家賃
         const rentElement = document.createElement("p");
         rentElement.className = "property-rent";
         rentElement.textContent = property.rent;
         cardElement.appendChild(rentElement);
+*/
 
         // 説明
         const descriptionElement = document.createElement("p");
